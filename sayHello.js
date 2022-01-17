cf014efc3beb809a7882910d2529c78513a0dcee
Ebragim780 <akbrly42@gmail.com>
2022-01-17 13:07:05 +0330

    add lodash & minor change to files

--- sayHello.js
+++ sayHello.js
@@ -1,3 +1,9 @@
 var colors = require('colors');
+var now = new Date().toLocaleTimeString();
 
-console.log(`Hello ${process. env.npm_package_config_name}`.italic.cyan);
+console.log(`Hello ${process.env.npm_package_config_name}`.italic.cyan);
+console.log(`started at ${now}`.bold.cyan);
+
+// var lodash = require('lodash');
+// var output = lodash.assign('Ebrahim','ali');
+// console.log(`${output}`);
